f4e269e4e60d89a86306936dfcb13b4085ccbb06
eunjee33 <eunjee3333@naver.com>
2024-12-06 10:41:37 +0900

    기능 3. 상대경로 -> 전체 URL, uniq_domains.txt 추가

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import sys
 from class3 import URLFinder
+import argparse
 
 def main():
-    # URL 입력받기
-    if len(sys.argv) != 3 or sys.argv[1] != "-f":
-        print("Usage: python test.py -f {js_file}")
-        return
-    
-    js_file = sys.argv[2]
+    parser = argparse.ArgumentParser(description="Usage: python test.py -f {js_file}")
+    parser.add_argument("-f", required=True, help="js_file")
+    args = parser.parse_args()
+
+    js_file = args.f
     
     # LinkFinderExecutor 실행
     URLFinder().find_url(js_file)
