78974a28f3e2be16d0fe4d64c343f91ff271ae0b
eunjee33 <eunjee3333@naver.com>
2024-12-09 09:13:52 +0900

    기능4. Endpoint 요청 자동화 구현

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 import sys
 from class3 import URLFinder
+from class4 import URLRequester
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="Usage: python test.py -f {js_file}")
+    """parser = argparse.ArgumentParser(description="Usage: python test.py -f {js_file}")
     parser.add_argument("-f", required=True, help="js_file")
     args = parser.parse_args()
 
     js_file = args.f
     
     # LinkFinderExecutor 실행
-    URLFinder().find_url(js_file)
+    url_finder = URLFinder()
+    url_finder.run(js_file)"""
+    
+    url_requester = URLRequester()
+    url_requester.run()
+
 
 if __name__ == "__main__":
     main()
